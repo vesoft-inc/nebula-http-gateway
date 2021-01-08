f663101c3bfd4d98fd49f9401c424d86783949a3
li Nico <37568394+NicolaCage@users.noreply.github.com>
2021-01-08 14:18:50 +0800

    mod: cloud go-api (#1)
    * mod: cloud go-api
    
    * mod: code review
    
    * mod: relog

--- routers/router.go
+++ routers/router.go
@@ -7,7 +7,7 @@ import (
 )
 
 func init() {
-	beego.Router("/api/db/connect", &controllers.DatabaseController{}, "POST:Connect")
-	beego.Router("/api/db/exec", &controllers.DatabaseController{}, "POST:Execute")
-	beego.Router("/api/db/disconnect", &controllers.DatabaseController{}, "POST:Disconnect")
+	beego.Router("/", &controllers.DatabaseController{}, "*:Home")
+	beego.Router("/api/nebula/connect", &controllers.DatabaseController{}, "POST:Connect")
+	beego.Router("/api/nebula/exec", &controllers.DatabaseController{}, "POST:Execute")
 }
