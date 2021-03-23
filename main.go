e0d948b1a08ea5721461ba2160447407038504f5
jerry.liang <8124783+nianiaJR@users.noreply.github.com>
2021-03-24 00:41:03 +0800

    fix: studio connect session

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	beego.BConfig.WebConfig.Session.SessionCookieLifeTime = 3600
+	beego.BConfig.WebConfig.Session.SessionCookieLifeTime = 0
+	beego.BConfig.WebConfig.Session.SessionGCMaxLifetime =  60 * 60 * 24
 	beego.BConfig.WebConfig.Session.SessionName = "nsid"
 	beego.BConfig.WebConfig.Session.SessionOn = true
 	beego.Run()
