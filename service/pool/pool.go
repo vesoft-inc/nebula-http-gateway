d36610e9d8d0850b0d7e95e997d6aba6bee459cf
Nut He <18328704+hetao92@users.noreply.github.com>
2021-07-13 16:04:50 +0800

    fix: update err info (#25)

--- service/pool/pool.go
+++ service/pool/pool.go
@@ -61,7 +61,7 @@ func NewConnection(address string, port int, username string, password string) (
 	// Initialize connectin pool
 	pool, err := nebula.NewConnectionPool(hostList, poolConfig, nebulaLog)
 	if err != nil {
-		return "", errors.New("Fail to initialize the connection pool")
+		return "", err
 	}
 	err = pool.Ping(hostList[0], 5000*time.Millisecond)
 	if err != nil {
