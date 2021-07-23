3034fb6d44112b0a5f7147b9ac831e4e9a07a44a
1uvu <zjh98@vip.qq.com>
2021-07-23 17:53:16 +0800

    Make filepath only be absolute

--- common/tools.go
+++ common/tools.go
@@ -1,24 +1,31 @@
 package common
 
 import (
+	"errors"
 	"os"
 	"path"
+	"path/filepath"
 	"strconv"
 	"syscall"
 )
 
-func CreateFileWithPerm(filepath string, permcode string) (*os.File, error) {
-	perm, err := strconv.ParseInt(permcode, 8, 64)
+func CreateFileWithPerm(filePath string, permCode string) (*os.File, error) {
+
+	if abs := filepath.IsAbs(filePath); !abs {
+		return nil, errors.New("file path should be absolute path")
+	}
+
+	perm, err := strconv.ParseInt(permCode, 8, 64)
 	if err != nil {
 		return nil, err
 	}
 	mask := syscall.Umask(0)
 	defer syscall.Umask(mask)
-	filedir := path.Dir(filepath)
+	filedir := path.Dir(filePath)
 	os.MkdirAll(filedir, os.FileMode(0770))
-	fd, err := os.OpenFile(filepath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, os.FileMode(perm))
+	fd, err := os.OpenFile(filePath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, os.FileMode(perm))
 	if err == nil {
-		os.Chmod(filepath, os.FileMode(perm))
+		os.Chmod(filePath, os.FileMode(perm))
 	}
 	return fd, err
 }
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"path/filepath"
 
 	_ "github.com/vesoft-inc/nebula-http-gateway/routers"
@@ -12,21 +14,27 @@ import (
 
 func main() {
 
-	//
-	// session config
-	//
+	/*
+		session config
+	*/
 	beego.BConfig.WebConfig.Session.SessionCookieLifeTime = 0
 	beego.BConfig.WebConfig.Session.SessionGCMaxLifetime = 60 * 60 * 24
 	beego.BConfig.WebConfig.Session.SessionName = "nsid"
 	beego.BConfig.WebConfig.Session.SessionOn = true
 
-	//
-	// logger config
-	//
+	/*
+		logger config
+	*/
 	logsPath := beego.AppConfig.String("logspath")
-	common.CreateFileWithPerm(logsPath, "0660")
+	absLogsPath, _ := filepath.Abs(logsPath)
+	_, err := common.CreateFileWithPerm(absLogsPath+"/", "0660")
+
+	if err != nil && os.IsNotExist(err) {
+		log.Fatalf("create file %s with error: %s", absLogsPath, err.Error())
+	}
+
 	logFilePath := filepath.Join(
-		logsPath,
+		absLogsPath,
 		"test.log",
 	)
 	beego.SetLogger("file", fmt.Sprintf(`{"filename":"%s","MaxSize":104857600,"perm":"0660"}`, logFilePath))
@@ -36,11 +44,16 @@ func main() {
 	// beego.SetLevel(beego.LevelInformational)
 	beego.SetLevel(beego.LevelDebug)
 
-	//
-	// importer file uploads config
-	//
+	/*
+		importer file uploads config
+	*/
 	uploadsPath := beego.AppConfig.String("uploadspath")
-	common.CreateFileWithPerm(uploadsPath, "0660")
+	absUploadsPath, _ := filepath.Abs(uploadsPath)
+	_, err = common.CreateFileWithPerm(absUploadsPath+"/", "0640")
+
+	if err != nil && os.IsNotExist(err) {
+		log.Fatalf("create file %s with error: %s", absLogsPath, err.Error())
+	}
 
 	beego.Run()
 }
