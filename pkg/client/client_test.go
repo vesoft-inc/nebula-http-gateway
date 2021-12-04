e221b92def694c723c4d9d3570b343b1bd7fed52
Katz <47083415+1uvu@users.noreply.github.com>
2021-12-04 19:07:43 +0800

    update client tests

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -2,11 +2,18 @@ package client
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
-	adapter "github.com/vesoft-inc/nebula-http-gateway/pkg/adapters/clientv2_6_0"
+
+	"github.com/vesoft-inc/nebula-http-gateway/pkg/adapters/clientv2_0_0_ga"
+	"github.com/vesoft-inc/nebula-http-gateway/pkg/adapters/clientv2_5_0"
+	"github.com/vesoft-inc/nebula-http-gateway/pkg/adapters/clientv2_5_1"
+	"github.com/vesoft-inc/nebula-http-gateway/pkg/adapters/clientv2_6_0"
 	"github.com/vesoft-inc/nebula-http-gateway/pkg/logger"
 	"github.com/vesoft-inc/nebula-http-gateway/pkg/types"
+	"io/ioutil"
 	"log"
+	"os"
 	"testing"
 	"time"
 )
@@ -27,9 +34,13 @@ var (
 		MinConnPoolSize: 0,
 		SslConfig:       nil,
 	}
-	sslConfig           = &tls.Config{} // todo
+
+	rootCA     = openAndReadFile("./tmp/test.ca.pem")
+	cert       = openAndReadFile("./tmp/test.client.crt")
+	privateKey = openAndReadFile("./tmp/test.client.key")
+
+	sslConfig           = testSslConfig()
 	configConfigWithSSL = types.ClientConfig{
-		Ver:             types.NewVersion("v2.6.0"),
 		TimeOut:         0 * time.Millisecond,
 		IdleTime:        0 * time.Millisecond,
 		MaxConnPoolSize: 10,
@@ -66,7 +77,7 @@ func TestV2_0_0_ga(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		resultSet, err := adapter.ResultSetWrapper(rset)
+		resultSet, err := clientv2_0_0_ga.ResultSetWrapper(rset)
 		if err != nil {
 			t.Error(err)
 		}
@@ -119,7 +130,7 @@ func TestV2_5_0(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		resultSet, err := adapter.ResultSetWrapper(rset)
+		resultSet, err := clientv2_5_0.ResultSetWrapper(rset)
 		if err != nil {
 			t.Error(err)
 		}
@@ -172,7 +183,7 @@ func TestV2_5_1(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		resultSet, err := adapter.ResultSetWrapper(rset)
+		resultSet, err := clientv2_5_1.ResultSetWrapper(rset)
 		if err != nil {
 			t.Error(err)
 		}
@@ -225,7 +236,7 @@ func TestV2_6_0(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		resultSet, err := adapter.ResultSetWrapper(rset)
+		resultSet, err := clientv2_6_0.ResultSetWrapper(rset)
 		if err != nil {
 			t.Error(err)
 		}
@@ -254,9 +265,10 @@ func TestV2_6_0(t *testing.T) {
 
 func TestV2_6_0_SSL(t *testing.T) {
 	log.Println("\n=> testing v2.6.0 with ssl")
-	config.Ver = types.NewVersion("v2.6.0")
+	configConfigWithSSL.Ver = types.NewVersion("v2.6.0")
 	fmt.Printf("test service address: %s:%d\n", address.Host, address.Port)
 
+	fmt.Println(configConfigWithSSL.SslConfig)
 	c, _ := NewClient([]types.HostAddress{address}, configConfigWithSSL, logger.DefaultLogger{})
 	log.Println("version test pass", c.Version())
 
@@ -278,7 +290,7 @@ func TestV2_6_0_SSL(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	resultSet, err := adapter.ResultSetWrapper(rset)
+	resultSet, err := clientv2_6_0.ResultSetWrapper(rset)
 	if err != nil {
 		t.Error(err)
 	}
@@ -301,4 +313,43 @@ func TestV2_6_0_SSL(t *testing.T) {
 
 	c.Close()
 	log.Println("client close test pass")
+}
+
+func testSslConfig() *tls.Config {
+	// generate the client certificate
+	clientCert, err := tls.X509KeyPair(cert, privateKey)
+	if err != nil {
+		panic(fmt.Sprintf("failed to get key pair%v", err))
+	}
+
+	// parse root CA pem and add into CA pool
+	rootCAPool := x509.NewCertPool()
+	ok := rootCAPool.AppendCertsFromPEM(rootCA)
+	if !ok {
+		panic("unable to append supplied cert into tls.Config, are you sure it is a valid certificate")
+	}
+
+	// set tls config
+	// InsecureSkipVerify is set to true for test purpose ONLY. DO NOT use it in production.
+	sslConfig := &tls.Config{
+		Certificates:       []tls.Certificate{clientCert},
+		RootCAs:            rootCAPool,
+		InsecureSkipVerify: true, // This is only used for testing
+	}
+
+	return sslConfig
+}
+
+func openAndReadFile(path string) []byte {
+	// open file
+	f, err := os.Open(path)
+	if err != nil {
+		panic(fmt.Sprintf("unable to open test file %s: %s", path, err))
+	}
+	// read file
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(fmt.Sprintf("unable to ReadAll of test file %s: %s", path, err))
+	}
+	return b
 }
