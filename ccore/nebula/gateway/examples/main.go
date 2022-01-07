2ce8c504004f4cc5f2a06a1affcff2e81bb630da
Katz <47083415+1uvu@users.noreply.github.com>
2022-01-07 15:37:47 +0800

    update test examples

--- ccore/nebula/gateway/examples/main.go
+++ ccore/nebula/gateway/examples/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	var (
-		address  = "192.168.8.169"
+		address  = "192.168.8.167"
 		port     = 9669
 		username = "root"
 		password = "123"
@@ -22,8 +22,20 @@ func main() {
 	log.Println(nsid)
 	defer dao.Disconnect(nsid)
 
-	gql := "show hosts;"
+	gql := "CREATE SPACE IF NOT EXISTS basic_example_space(vid_type=FIXED_STRING(20)); " +
+		"USE basic_example_space;" +
+		"CREATE TAG IF NOT EXISTS person(name string, age int);" +
+		"CREATE EDGE IF NOT EXISTS like(likeness double);"
 	res, p, err := dao.Execute(nsid, gql, nil)
+	if err != nil {
+		log.Println("error: ", err)
+		if p != nil {
+			log.Fatal(p)
+		}
+	}
+
+	gql = "DROP SPACE IF EXISTS basic_example_space;"
+	res, p, err = dao.Execute(nsid, gql, nil)
 	if err != nil {
 		log.Println("error: ", err)
 		if p != nil {
