9addd2586e2ff3a805acec0f056e7d0b432781fd
HarrisChu <1726587+HarrisChu@users.noreply.github.com>
2022-02-21 11:26:44 +0800

    close the invalid connection

--- ccore/nebula/driver.go
+++ ccore/nebula/driver.go
@@ -89,11 +89,13 @@ func (d *driverGraph) open(driver types.Driver) error {
 	}
 
 	if err = graphClientDriver.VerifyClientVersion(); err != nil {
+		graphClientDriver.Close()
 		return err
 	}
 
 	resp, err := graphClientDriver.Authenticate(d.username, d.password)
 	if err != nil {
+		graphClientDriver.Close()
 		return err
 	}
 
